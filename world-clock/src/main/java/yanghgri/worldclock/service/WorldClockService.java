74cf8de47a569b29d1aada0741cc10c34d194475
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-07-19 12:42:21 +0800

    将常量挪到二方库中

--- world-clock/src/main/java/yanghgri/worldclock/service/WorldClockService.java
+++ world-clock/src/main/java/yanghgri/worldclock/service/WorldClockService.java
@@ -2,23 +2,18 @@ package yanghgri.worldclock.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import yanghgri.common.constant.ZonedDateTimeConstant;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @Service
 public class WorldClockService {
     //配置好的时区集合
     private final Set<ZoneId> zoneIdSet;
-    //默认的时间格式化器，放在二方库中更优雅
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
-    //表示法，选这个就是了
-    private static final TextStyle TEXT_STYLE = TextStyle.FULL;
-    //由JVM宿主机环境决定当前地理地区
-    public static final Locale DEFAULT_LOCALE = Locale.getDefault();
 
     @Autowired
     public WorldClockService(Set<ZoneId> zoneIdSet) {
@@ -29,7 +24,7 @@ public class WorldClockService {
         Map<String, String> zonedDateTimeMap = new HashMap<>(zoneIdSet.size());
         ZonedDateTime now = ZonedDateTime.now();
         //withZone()是返回个新的对象而不是给某个成员变量赋值，不用担心线程安全问题
-        zoneIdSet.forEach(zoneId -> zonedDateTimeMap.put(zoneId.getDisplayName(TEXT_STYLE, DEFAULT_LOCALE), DATE_TIME_FORMATTER.withZone(zoneId).format(now)));
+        zoneIdSet.forEach(zoneId -> zonedDateTimeMap.put(zoneId.getDisplayName(ZonedDateTimeConstant.DEFAULT_TEXT_STYLE, ZonedDateTimeConstant.DEFAULT_LOCALE), ZonedDateTimeConstant.DEFAULT_DATE_TIME_FORMATTER.withZone(zoneId).format(now)));
         return zonedDateTimeMap;
     }
 }

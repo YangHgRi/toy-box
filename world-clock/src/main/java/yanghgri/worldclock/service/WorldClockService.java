69e7c6f025d7e6239c41bef5e06cbff4a31377ca
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-07-19 11:28:46 +0800

    补充注释，另外上次提交的日志写错了，是展示不是暂时

--- world-clock/src/main/java/yanghgri/worldclock/service/WorldClockService.java
+++ world-clock/src/main/java/yanghgri/worldclock/service/WorldClockService.java
@@ -11,9 +11,13 @@ import java.util.*;
 
 @Service
 public class WorldClockService {
+    //配置好的时区集合
     private final Set<ZoneId> zoneIdSet;
+    //默认的时间格式化器，放在二方库中更优雅
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
+    //表示法，选这个就是了
     private static final TextStyle TEXT_STYLE = TextStyle.FULL;
+    //由JVM宿主机环境决定当前地理地区
     public static final Locale DEFAULT_LOCALE = Locale.getDefault();
 
     @Autowired
@@ -24,6 +28,7 @@ public class WorldClockService {
     public Map<String, String> show() {
         Map<String, String> zonedDateTimeMap = new HashMap<>(zoneIdSet.size());
         ZonedDateTime now = ZonedDateTime.now();
+        //withZone()是返回个新的对象而不是给某个成员变量赋值，不用担心线程安全问题
         zoneIdSet.forEach(zoneId -> zonedDateTimeMap.put(zoneId.getDisplayName(TEXT_STYLE, DEFAULT_LOCALE), DATE_TIME_FORMATTER.withZone(zoneId).format(now)));
         return zonedDateTimeMap;
     }

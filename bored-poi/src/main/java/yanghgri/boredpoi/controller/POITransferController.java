ba6f17a66c48c7b14ab208cd6adc1c09005c0d5f
YangHgRi <57426791+YangHgRi@users.noreply.github.com>
2022-07-21 17:43:16 +0800

    编写读取Excel接口

--- bored-poi/src/main/java/yanghgri/boredpoi/controller/POITransferController.java
+++ bored-poi/src/main/java/yanghgri/boredpoi/controller/POITransferController.java
@@ -1,10 +1,14 @@
 package yanghgri.boredpoi.controller;
 
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import yanghgri.boredpoi.service.ExcelService;
 import yanghgri.devform.base.enums.SpecialMIMEType;
 
@@ -27,6 +31,16 @@ public class POITransferController {
             response.setContentType(SpecialMIMEType.EXCEL_XLSX.getContent());
             response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=example.xlsx");
             workbook.write(outputStream);
+        }
+    }
+
+    @PostMapping("/excel/read")
+    public String readExcel(@RequestPart("file") MultipartFile file) {
+        try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
+            return excelService.readExcelFile(workbook);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+            return null;
         }
     }
 }
--- bored-poi/src/main/java/yanghgri/boredpoi/service/ExcelService.java
+++ bored-poi/src/main/java/yanghgri/boredpoi/service/ExcelService.java
@@ -12,9 +12,16 @@ public class ExcelService {
     public Workbook createExcelFile() {
         Workbook workbook = new XSSFWorkbook();
         Sheet sheet = workbook.createSheet("第一页");
-        Row row = sheet.createRow(9);
+        Row row = sheet.createRow(0);
         Cell cell = row.createCell(0);
-        cell.setCellValue("哈喽，第十行，第一列");
+        cell.setCellValue("哈喽，第一页，第一行，第一列");
         return workbook;
+    }
+
+    public String readExcelFile(Workbook workbook) {
+        Sheet sheet = workbook.getSheet("第一页");
+        Row row = sheet.getRow(0);
+        Cell cell = row.getCell(0);
+        return cell.getStringCellValue();
     }
 }
